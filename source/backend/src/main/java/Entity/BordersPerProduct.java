6b89324e32ffce753e8e4f2869a41007682e0c69
Kevin Strijbos <strijboskevin@hotmail.com>
2016-09-30 10:10:41 +0200

    add new entity

---
+++ source/backend/src/main/java/Entity/BordersPerProduct.java
@@ -0,0 +1,28 @@
+package Entity;
+
+public class BordersPerProduct {
+
+	private int product_id;
+	private int border_value_variable;
+	private float border_value_number;
+	
+	public int getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+	public int getBorder_value_variable() {
+		return border_value_variable;
+	}
+	public void setBorder_value_variable(int border_value_variable) {
+		this.border_value_variable = border_value_variable;
+	}
+	public float getBorder_value_number() {
+		return border_value_number;
+	}
+	public void setBorder_value_number(float border_value_number) {
+		this.border_value_number = border_value_number;
+	}
+	
+}
---
+++ source/backend/src/main/java/Entity/Product.java
@@ -0,0 +1,21 @@
+package Entity;
+
+public class Product {
+
+	private int product_id;
+	private String description;
+	
+	public int getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
