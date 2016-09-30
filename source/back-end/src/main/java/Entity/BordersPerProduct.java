0732e4d84c973a7a20a1580f19efc5b419cbe27e
Kevin Strijbos <strijboskevin@hotmail.com>
2016-09-30 11:30:20 +0200

    add table annotation

--- source/back-end/src/main/java/Entity/BordersPerProduct.java
+++ source/back-end/src/main/java/Entity/BordersPerProduct.java
@@ -4,8 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="BordersPerProduct")
 public class BordersPerProduct {
 
 	@Id
