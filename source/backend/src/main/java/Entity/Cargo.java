6ed4b73f23192973c9ed1220b7c03d3b4f37ab17
Kevin Strijbos <strijboskevin@hotmail.com>
2016-09-30 09:26:23 +0200

    ...

--- source/backend/src/main/java/Entity/Cargo.java
+++ source/backend/src/main/java/Entity/Cargo.java
@@ -2,13 +2,35 @@ package Entity;
 
 class Cargo {
 
-	private int cargo_id;
-	private int sensor_id;
-	private int chauffeur_id;
-	private int bestemming_id;
-	private float gewicht;
+	private int _cargoId;
+	private int _sensorId;
+	private int _driverId;
+	private int _destinationId;
+	private float _weight;
 	
-	private int getCargoId() { return cargo_id; }
-	private int getSensorId() {return sensor_id; }
+	private int getCargoId() { return _cargoId; }
+	private int getSensorId() { return _sensorId; }
+	private int getDriverId() { return _driverId; }
+	private int getDestinationId() { return _destinationId; }
+	private float getWeight() { return _weight; }
 	
+	private void setCargoId(int id) {
+		_cargoId = id;
+	}
+	
+	private void setSensorId(int id) {
+		_sensorId = id;
+	}
+	
+	private void setDriverId(int id) {
+		_driverId = id;
+	}
+	
+	private void setDestinationId(int id) {
+		_destinationId = id;
+	}
+	
+	private void setWeight(float weight) {
+		_weight = weight;
+	}
 }
