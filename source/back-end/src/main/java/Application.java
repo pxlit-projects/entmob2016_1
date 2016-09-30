1fb6a71e76c2bf12beae1fcd4c0eff8ae15e8590
Kevin Strijbos <strijboskevin@hotmail.com>
2016-09-30 14:09:48 +0200

    merge

--- source/back-end/src/main/java/Application.java
+++ source/back-end/src/main/java/Application.java
@@ -1,10 +1,5 @@
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-
 import be.pxl.backend.dao.BordersPerProductDAO;
 import be.pxl.backend.dao.BordersPerProductDAOImpl;
 import be.pxl.backend.entity.BordersPerProduct;
--- source/back-end/src/main/java/be/pxl/backend/dao/BordersPerProductDAOImpl.java
+++ source/back-end/src/main/java/be/pxl/backend/dao/BordersPerProductDAOImpl.java
@@ -2,11 +2,6 @@ package be.pxl.backend.dao;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-
 import be.pxl.backend.entity.BordersPerProduct;
 
 public class BordersPerProductDAOImpl implements BordersPerProductDAO {
