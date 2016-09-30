5f441bb5b70a4b7f80d65696c9e3b2f82950af67
Kevin Strijbos <strijboskevin@hotmail.com>
2016-09-30 09:20:31 +0200

    push for brum

---
+++ source/backend/src/main/java/Entity/Cargo.java
@@ -0,0 +1,14 @@
+package Entity;
+
+class Cargo {
+
+	private int cargo_id;
+	private int sensor_id;
+	private int chauffeur_id;
+	private int bestemming_id;
+	private float gewicht;
+	
+	private int getCargoId() { return cargo_id; }
+	private int getSensorId() {return sensor_id; }
+	
+}
