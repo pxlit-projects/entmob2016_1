daf5e2187adb9270e5fc3b009b8f43749267a98a
Kevin Strijbos <strijboskevin@hotmail.com>
2016-10-03 09:27:58 +0200

    change application.properties

--- source/back-end/src/main/java/be/pxl/backend/Application.java
+++ source/back-end/src/main/java/be/pxl/backend/Application.java
@@ -8,9 +8,10 @@ import be.pxl.backend.repository.ProductRepository;
 public class Application {
 
 	public static void main (String[] args) {
-		ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(AppConfig.class, args);
 		ProductRepository repo = ctx.getBean(ProductRepository.class);
 		Product product = new Product();
 		product.setDescription("HDD");
+		repo.save(product);
 	}
 }
--- source/back-end/src/main/java/be/pxl/backend/repository/ProductRepository.java
+++ source/back-end/src/main/java/be/pxl/backend/repository/ProductRepository.java
@@ -1,8 +1,10 @@
 package be.pxl.backend.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 import be.pxl.backend.entity.*;
 
+@Component
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
 }
