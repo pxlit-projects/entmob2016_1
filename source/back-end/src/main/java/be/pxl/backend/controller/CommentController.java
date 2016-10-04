6163ba26cf28f28f78563e5968d1e7a46d48ae9b
BramVV <bram.vanvleymen@student.pxl.be>
2016-10-04 17:39:50 +0200

    added Comment to api

---
+++ source/back-end/src/main/java/be/pxl/backend/controller/CommentController.java
@@ -0,0 +1,46 @@
+package be.pxl.backend.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import be.pxl.backend.entity.Cargo;
+import be.pxl.backend.entity.Comment;
+import be.pxl.backend.service.CargoService;
+import be.pxl.backend.service.CommentService;
+
+@RestController
+@RequestMapping("/comments")
+public class CommentController {
+	
+	@Autowired
+	private CommentService service;
+	
+	@RequestMapping(value = "/get/{id}", method = RequestMethod.GET, produces="application/json")
+	public Comment getCommentById(@PathVariable("id") int id) {
+		return service.find(id);
+	}
+	
+	@RequestMapping(value = "/all", method = RequestMethod.GET, produces="application/json")
+	public List<Comment> getAllComments() {
+		return service.all();
+	}
+	
+	@RequestMapping(value = "/add/{cargo}", method=RequestMethod.POST)
+	public void addComment(@RequestBody Comment comment) {
+		service.persist(comment);
+	}
+	
+	@RequestMapping(value = "/delete/{id}", method=RequestMethod.DELETE)
+	public void deleteComment(@PathVariable("id") int id) {
+		service.delete(id);
+	}
+	
+	@RequestMapping(value = "/update/{id}", method=RequestMethod.PUT)
+	public void updateComment(@PathVariable("id") int id, @RequestBody Comment comment) {
+		service.update(id, comment);
+	}
+}
