b05e4705a1607e01629d6ad50ab9975301819199
BramVV <bram.vanvleymen@student.pxl.be>
2016-10-04 18:36:25 +0200

    stabilisationsPerCargo Added

---
+++ source/back-end/src/main/java/be/pxl/backend/service/StabilisationsPerCargoService.java
@@ -0,0 +1,36 @@
+package be.pxl.backend.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import be.pxl.backend.entity.StabilisationsPerCargo;
+import be.pxl.backend.repository.StabilisationsPerCargoRepository;
+
+@Component
+public class StabilisationsPerCargoService {
+
+	@Autowired
+	private StabilisationsPerCargoRepository repo;
+	
+	public StabilisationsPerCargo find(int id) {
+		return repo.findOne(id);
+	}
+	
+	public List<StabilisationsPerCargo> all() {
+		return repo.findAll();
+	}
+	
+	public void persist(StabilisationsPerCargo stabilisationsPerCargo) {
+		repo.save(stabilisationsPerCargo);
+	}
+	
+	public void delete(int id) {
+		repo.delete(id);
+	}
+	
+	public void update(int id, StabilisationsPerCargo stabilisationsPerCargo) {
+		this.delete(id);
+		this.persist(stabilisationsPerCargo);
+	}
+	
+}
