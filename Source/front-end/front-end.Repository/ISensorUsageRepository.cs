908d8f42d1a9323ca039277c491c7846ecb3b063
JoranClaessens <joran.claessens@student.pxl.be>
2016-10-07 11:30:31 +0200

    Fixed ISensorUsageRepository

--- Source/front-end/front-end.Repository/ISensorUsageRepository.cs
+++ Source/front-end/front-end.Repository/ISensorUsageRepository.cs
@@ -9,8 +9,8 @@ namespace front_end.Repository
 {
     public interface ISensorUsageRepository
     {
-        Task<IEnumerable<SensorUsage>> GetAllSensorUsage();
-        Task<SensorUsage> GetSensorById(int id);
+        Task<IEnumerable<SensorUsage>> GetAllSensorUsages();
+        Task<SensorUsage> GetSensorUsageById(int id);
         void AddSensorUsage(SensorUsage sensorUsage);
         void UpdateSensorUsage(SensorUsage sensorUsage);
         void DeleteSensorUsage(int id);
