e7544a71f4c3d80f9e19db3f028a4ffc2c864430
BramVV <bram.vanvleymen@student.pxl.be>
2016-10-07 17:20:35 +0200

    Login Screen app

---
+++ Source/Mobile-App/Mobile-App/Mobile_App/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Mobile_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public void LoginClicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
