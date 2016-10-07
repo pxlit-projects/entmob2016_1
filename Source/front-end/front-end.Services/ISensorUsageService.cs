9acd6bcd2c38fadbbfe9ea37a903079bc5bf88a0
JoranClaessens <joran.claessens@student.pxl.be>
2016-10-07 10:14:51 +0200

    Added blank service interfaces

---
+++ Source/front-end/front-end.Services/ISensorUsageService.cs
@@ -0,0 +1,13 @@
+﻿using front_end.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace front_end.Services
+{
+    public interface ISensorUsageService
+    {
+    }
+}
