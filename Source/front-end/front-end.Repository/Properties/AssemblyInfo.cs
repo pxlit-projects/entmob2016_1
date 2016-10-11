30ce15f5322679b0faa5799d8f7a074c55892684
JoranClaessens <joran.claessens@student.pxl.be>
2016-10-11 09:47:36 +0200

    fixed front-end solution

---
+++ Source/front-end/front-end.Repository/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("front-end.Repository")]
+[assembly: AssemblyDescription("Repository")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("PXL")]
+[assembly: AssemblyProduct("front_end.Repository.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("972f1887-da1c-406e-b8f2-50a79ac3d7ec")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
