c36141fa70ae9b2dfa95940e6c5177fc24797828
Sacha Reyskens <11400982@student.pxl.be>
2016-10-14 14:43:34 +0200

    Adding new viewmodels

---
+++ Source/frontend/frontend.App/ViewModelLocator.cs
@@ -0,0 +1,47 @@
+﻿using frontend.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using frontend.App.ViewModels;
+
+namespace frontend.App
+{
+    public class ViewModelLocator
+    {
+        private static ISensorService sensorService = new SensorService();
+        private static IProductService productService = new ProductService();
+        private static IEmployeeService employeeService = new EmployeeService();
+
+        private static SensorViewModel sensorViewModel = new SensorViewModel(sensorService);
+        private static ProductViewModel productViewModel = new ProductViewModel(productService);
+        private static DriverViewModel driverViewModel = new DriverViewModel(employeeService);
+
+        public static DriverViewModel DriverViewModel
+        {
+            get
+            {
+                return driverViewModel;
+            }
+        }
+
+        public static ProductViewModel ProductViewModel
+        {
+            get
+            {
+                return productViewModel;
+            }
+        }
+
+        public static SensorViewModel SensorViewModel
+        {
+            get
+            {
+                return sensorViewModel;
+            }
+        }
+
+
+    }
+}
