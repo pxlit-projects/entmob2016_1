66f9e805d7d3b3ec718cc8c5185c67b490c7e675
JoranClaessens <joran.claessens@student.pxl.be>
2016-10-14 10:04:17 +0200

    Added DriversViewModel

---
+++ Source/front-end/front-end.App/ViewModel/DriversViewModel.cs
@@ -0,0 +1,66 @@
+﻿using front_end.Domain;
+using front_end.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace front_end.App.ViewModel
+{
+    class DriversViewModel : INotifyPropertyChanged
+    {
+        IEmployeeService service;
+        private ObservableCollection<Employee> drivers;
+        private Employee selectedDriver;
+
+        public DriversViewModel(IEmployeeService service)
+        {
+            service = this.service;
+            LoadData();
+        }
+
+        public ObservableCollection<Employee> Drivers
+        {
+            get
+            {
+                return drivers;
+            }
+            set
+            {
+                drivers = value;
+                RaisePropertyChanged("Drivers");
+            }
+        }
+
+        public Employee SelectedDriver
+        {
+            get
+            {
+                return selectedDriver;
+            }
+            set
+            {
+                selectedDriver = value;
+                RaisePropertyChanged("SelectedDriver");
+            }
+        }
+
+        private void LoadData()
+        {
+            var dummy = service.All().OrderBy(d => d.employee_id);
+            Drivers = new ObservableCollection<Employee>(dummy);
+            SelectedDriver = drivers.ElementAt(0);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string v)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(v));
+        }
+    }
+}
