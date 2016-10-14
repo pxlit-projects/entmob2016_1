bb7afcdff0e6986a0577755d7870e16d5ea50eaf
Sacha Reyskens <11400982@student.pxl.be>
2016-10-14 15:42:16 +0200

    Joran voor jou enjoy le bugs

---
+++ Source/frontend/frontend.App/ViewModels/ProductViewModel.cs
@@ -0,0 +1,65 @@
+﻿using frontend.Domain;
+using frontend.Service;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frontend.App.ViewModels
+{
+    public class ProductViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private IProductService service = new ProductService();
+
+        private ObservableCollection<Product> products;
+
+        public ProductViewModel(IProductService service)
+        {
+            this.service = service;
+            LoadData();
+        }
+
+        public ObservableCollection<Product> Products
+        {
+            get
+            {
+                return products;
+            }
+            set
+            {
+                products = value;
+                RaisePropertyChanged("Products");
+            }
+        }
+
+        private Product selectedProduct;
+
+        public Product SelectedProduct
+        {
+            get
+            {
+                return selectedProduct;
+            }
+            set
+            {
+                selectedProduct = value;
+                RaisePropertyChanged("SelectedProduct");
+            }
+        }
+
+        private void LoadData()
+        {
+            var productslist = service.All().OrderBy(d => d.product_id);
+            Products = new ObservableCollection<Product>(productslist);
+            SelectedProduct = products.ElementAt(0);
+        }
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Source/frontend/frontend.App/ViewModels/SensorViewModel.cs
@@ -0,0 +1,66 @@
+﻿using frontend.Domain;
+using frontend.Service;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frontend.App.ViewModels
+{
+    public class SensorViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private ISensorService service = new SensorService();
+
+        private ObservableCollection<Sensor> sensors;
+
+        public SensorViewModel(ISensorService service)
+        {
+            this.service = service;
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            var sensorsList = service.All().OrderBy(d => d.sensor_id);
+            Sensors = new ObservableCollection<Sensor>(sensorsList);
+            SelectedSensor = sensors.ElementAt(0);
+        }
+
+        public ObservableCollection<Sensor> Sensors
+        {
+            get
+            {
+                return sensors;
+            }
+            set
+            {
+                sensors = value;
+                RaisePropertyChanged("Sensors");
+            }
+        }
+
+        private Sensor selectedSensor;
+
+        public Sensor SelectedSensor
+        {
+            get
+            {
+                return selectedSensor;
+            }
+            set
+            {
+                selectedSensor = value;
+                RaisePropertyChanged("SelectedSensor");
+            }
+        }
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
