98e71fd4d20ef4831a8626259dc430466cd2e077
Sacha Reyskens <11400982@student.pxl.be>
2016-10-19 14:33:18 +0200

    Updating viewmodels and customcommands

--- Source/frontend/MainApp/ViewModelLocator.cs
+++ Source/frontend/MainApp/ViewModelLocator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using frontend.Service;
+using MainApp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,49 @@ using System.Threading.Tasks;
 
 namespace MainApp
 {
-    class ViewModelLocator
+    public class ViewModelLocator
     {
+        private static ISensorService sensorService = new SensorService();
+        private static IProductService productService = new ProductService();
+        private static IEmployeeService employeeService = new EmployeeService();
+
+
+        private static SensorViewModel sensorViewModel = new SensorViewModel(sensorService);
+        private static ProductViewModel productViewModel = new ProductViewModel(productService);
+        private static DriverViewModel driverViewModel = new DriverViewModel(employeeService);
+        private static MainViewModel mainViewModel = new MainViewModel();
+
+        public static DriverViewModel DriverViewModel
+        {
+            get
+            {
+                return driverViewModel;
+            }
+        }
+
+        public static ProductViewModel ProductViewModel
+        {
+            get
+            {
+                return productViewModel;
+            }
+        }
+
+        public static SensorViewModel SensorViewModel
+        {
+            get
+            {
+                return sensorViewModel;
+            }
+        }
+
+        public static MainViewModel MainViewModel
+        {
+            get
+            {
+                return mainViewModel;
+            }
+        }
+
     }
 }
