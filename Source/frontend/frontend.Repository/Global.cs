614476d7ecb27ed13d1508e3a97685dbd6b572f9
Sacha Reyskens <11400982@student.pxl.be>
2016-10-19 17:01:55 +0200

    Fixing brams fucked up merge

--- Source/frontend/frontend.Repository/Global.cs
+++ Source/frontend/frontend.Repository/Global.cs
@@ -8,14 +8,11 @@ namespace frontend.Repository
 {
     class Global
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        public static string IP_ADRESS = "http://10.84.134.171:8080/";
-=======
+
+        
+
         public static String IP_ADRESS = "http://localhost:8080";
->>>>>>> origin/master
-=======
-        public static String IP_ADRESS = "http://localhost:8080";
->>>>>>> 0ef0c13002dce119e938fae6487b08c26a99f21f
+
+
     }
 }
