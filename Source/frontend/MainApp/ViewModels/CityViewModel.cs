a85581a1c2a10c84e6b1b13f0d17c8056089c218
Sacha Reyskens <11400982@student.pxl.be>
2016-10-21 14:04:58 +0200

    Adding new models

---
+++ Source/frontend/MainApp/ViewModels/CityViewModel.cs
@@ -0,0 +1,108 @@
+﻿using frontend.Domain;
+using frontend.Service;
+using MainApp.Utility;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Popups;
+
+namespace MainApp.ViewModels
+{
+    public class CityViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<City> cities;
+        private City selectedCity;
+
+        public ICommand UpdateCommand { get; set; }
+        
+        public ICommand ShowCityDialogCommand { get; set; }
+
+        private ICityService service;
+
+        public CityViewModel(ICityService service)
+        {
+            this.service = service;
+            LoadData();
+            LoadCommands();
+        }
+
+        public ObservableCollection<City> Cities
+        {
+            get
+            {
+                return cities;
+            }
+            set
+            {
+                cities = value;
+                RaisePropertyChanged("Cities");
+            }
+        }
+
+        public City SelectedCity
+        {
+            get
+            {
+                return selectedCity;
+            }
+            set
+            {
+                selectedCity = value;
+                RaisePropertyChanged("SelectedCity");
+            }
+        }
+
+        private void LoadData()
+        {
+            var dummy = service.All().OrderBy(d => d.postal_code);
+            Cities = new ObservableCollection<City>(dummy);
+            SelectedCity = cities.ElementAt(0);
+        }
+
+        private void LoadCommands()
+        {
+            UpdateCommand = new CustomCommand(Update, CanUpdate);
+            
+            ShowCityDialogCommand = new CustomCommand(ShowCityDialog, null);
+        }
+
+        private bool CanUpdate(object obj)
+        {
+            return SelectedCity != null;
+        }
+
+        
+
+        private void Update(object obj)
+        {
+            service.Update(SelectedCity);
+            LoadData();
+        }
+
+        
+
+
+        public void ShowCityDialog(object obj)
+        {
+            //var logindialog = new AddCargoDialog();
+            //var result = await logindialog.ShowAsync();
+            //if (result == ContentDialogResult.Primary)
+            //{
+            //    LoadData();
+            //}
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string v)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(v));
+        }
+    }
+}
---
+++ Source/frontend/MainApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,107 @@
+﻿using frontend.Domain;
+using frontend.Service;
+using MainApp.Utility;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Popups;
+
+namespace MainApp.ViewModels
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Login> logins;
+        private Login selectedLogin;
+
+        public ICommand UpdateCommand { get; set; }
+        public ICommand DetailsCommand { get; set; }
+        
+
+        private ILoginService service;
+
+        public LoginViewModel(ILoginService service)
+        {
+            this.service = service;
+            LoadData();
+            LoadCommands();
+        }
+
+        public ObservableCollection<Login> Logins
+        {
+            get
+            {
+                return logins;
+            }
+            set
+            {
+                logins = value;
+                RaisePropertyChanged("Logins");
+            }
+        }
+
+        public Login SelectedLogin
+        {
+            get
+            {
+                return selectedLogin;
+            }
+            set
+            {
+                selectedLogin = value;
+                RaisePropertyChanged("SelectedLogin");
+            }
+        }
+
+        private void LoadData()
+        {
+            var dummy = service.All().OrderBy(d => d.login_id);
+            Logins = new ObservableCollection<Login>(dummy);
+            SelectedLogin = logins.ElementAt(0);
+        }
+
+        private void LoadCommands()
+        {
+            UpdateCommand = new CustomCommand(Update, CanUpdate);
+            DetailsCommand = new RelayCommand<MessageDialog>(ShowDetails, null);
+           
+        }
+
+        private bool CanUpdate(object obj)
+        {
+            return SelectedLogin != null;
+        }
+
+        private bool CanChangeStatus(object obj)
+        {
+            return SelectedLogin != null;
+        }
+
+        private void Update(object obj)
+        {
+            service.Update(SelectedLogin);
+            LoadData();
+        }
+
+        public void ShowDetails(MessageDialog message)
+        {
+
+        }
+
+
+       
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string v)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(v));
+        }
+    }
+}
+
