26677b007cf82223db574bafc6482c32d7512db8
JoranClaessens <joran.claessens@student.pxl.be>
2016-10-28 16:09:26 +0200

    Changed layout

--- Source/frontend/MainApp/ViewModels/SensorVIewModel.cs
+++ Source/frontend/MainApp/ViewModels/SensorVIewModel.cs
@@ -75,7 +75,7 @@ namespace MainApp.ViewModels
 
         public void ShowSensorUsage(object obj)
         {
-            var list = usageService.All().Where(d => d.Sensor == SelectedSensor);
+            var list = usageService.All().Where(d => d.Sensor.Sensor_id == SelectedSensor.Sensor_id);
             SensorUsages = new ObservableCollection<SensorUsage>(list);
         }
         public ObservableCollection<Sensor> Sensors
