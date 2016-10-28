0742ca68a0690976afc89296ed0e433e822aaf49
BramVV <bram.vanvleymen@student.pxl.be>
2016-10-28 16:03:00 +0200

    Finally got the data out of the sensor!

---
+++ Source/Mobile-App/Mobile-App/Mobile_App/Services/VariableMessage.cs
@@ -0,0 +1,15 @@
+﻿using Robotics.Mobile.Core.Bluetooth.LE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile_App.Services
+{
+    public class VariableMessage
+    {
+        public IDevice connectedDevice { get; set; }
+        public IAdapter adapter { get; set; }
+    }
+}
---
+++ Source/Mobile-App/Mobile-App/Mobile_App/View/HomeView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using JoesBurgerStore;
+using Mobile_App.Services;
+using Robotics.Mobile.Core.Bluetooth.LE;
+using Mobile_App.ViewModel;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using System.Diagnostics;
+using System;
+
+namespace Mobile_App.View
+{
+    public partial class HomeView : ContentPage
+    {
+        private readonly HomeViewModel viewModel;
+        public HomeView()
+        {
+            InitializeComponent();
+            viewModel = AppContainer.Container.Resolve<ViewModel.HomeViewModel>();
+            this.BindingContext = viewModel;
+        }
+    }
+}
