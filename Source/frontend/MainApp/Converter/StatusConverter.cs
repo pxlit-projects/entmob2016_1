5f259accb3c22ece5e42baab9051790a49790c0f
Joran Claessens <11400919@student.pxl.be>
2016-11-21 11:58:25 +0100

    Added new icons

---
+++ Source/frontend/MainApp/Converter/StatusConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace MainApp.Converter
+{
+    public class StatusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool statusValue = (bool)value;
+
+            if (statusValue == true)
+            {
+                return "Actief";
+            }
+            else
+            {
+                return "Non-actief";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
